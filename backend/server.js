3d07bb1cdb07db2bad78bc9c6852d0a48c375734
nakkul bhandari <nakkul1364@gmail.com>
2026-03-14 01:25:59 +0530

    some more fixes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const frontendPath = path.join(__dirname, "../../frontend/dist");
 
 app.use(express.static(frontendPath));
 
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
