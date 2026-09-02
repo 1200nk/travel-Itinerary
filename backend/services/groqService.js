66f13e4b5a981551035f14bb206b67d9e649e25a
nakkul bhandari <nakkul1364@gmail.com>
2026-09-02 16:23:56 +0530

    change model

--- backend/services/groqService.js
+++ backend/services/groqService.js
@@ -3,7 +3,7 @@ import { tripPrompt } from "../prompts/tripPrompt.js";
 
 export async function generateTripPlan(userPrompt) {
   const completion = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
+    model: "llama-3.1-8b-instant",
     messages: [
       {
         role: "system",
